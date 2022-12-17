151e49bc9bbfb7baa9af00c85141f623f1923e2f
Luana <lugamoreira@gmail.com>
2022-12-17 13:34:52 -0500

    commit tree 749c6b9ab967be3165b5abe5577fa4839714296c

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(friends , event) {
+    const messages = [];
+  let i = 0; 
+  while (i < friends.length) {
+   messages.push(`Thank you, ${friends[i]}, for the wonderful ${event} gift!`);
+    i++;
+  }
+  return messages;
+};
+    
+
+
+function countDown() {
+    let count = 10 
+    while (count >= 0)
+        console.log(count--);
+
+}
